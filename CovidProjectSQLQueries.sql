5b13906d66e6f3aa148fb3e693a8c377b86ed750
Vishwas Chaudhary <143931674+tumbleweed11@users.noreply.github.com>
2023-12-12 12:09:16 +0530

    Update CovidProjectSQLQueries.sql

--- CovidProjectSQLQueries.sql
+++ CovidProjectSQLQueries.sql
@@ -121,27 +121,52 @@ FROM #PercentPopulationVaccinated
 
 
 
--- Creating View to store data for later visualisation.
+/*
 
---1
+Queries used for Tableau Project
 
-CREATE VIEW PercentPopulationVaccinated AS
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-,SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
-FROM CovidProject..CovidDeaths dea
-JOIN CovidProject..CovidVaccinations vac
-ON  dea.location = vac.location
-	AND dea.date = vac.date
-WHERE dea.continent IS NOT NULL
+*/
 
 
---2
 
-CREATE VIEW GlobalNumbers AS
-SELECT  SUM(new_cases) AS total_cases,
-		SUM(CAST(new_deaths AS bigint)) AS total_deaths, 
-		CASE
-			WHEN SUM(new_cases) = 0 THEN NULL
-			ELSE SUM(CAST(new_deaths AS bigint))/SUM(new_cases)*100
-		END AS DeathPercentage
-FROM CovidProject..CovidDeaths
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
